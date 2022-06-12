0a01a59acfb11dd8a52a1750f1220ab76a4c454f
age liu <ageryuu@gmail.com>
2022-06-12 20:51:50 +0800

    add some functions

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from  '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,18 @@ import { Hero } from  '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstrom'
-  };
+  heroes = HEROES; // html *ngFor="let hero of heroes"
+
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //  (click)="onSelect(hero)"
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
