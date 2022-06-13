1705ce8b3308499ab2fb00e71f1b660a85efc60e
age liu <ageryuu@gmail.com>
2022-06-13 22:14:16 +0800

    use subscribe and obsevable object

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from  '../hero';
-import { HEROES } from '../mock-heroes';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +10,15 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES; // html *ngFor="let hero of heroes"
+  heroes: Hero[] = []; // html *ngFor="let hero of heroes"
 
   selectedHero?: Hero;
 
-  constructor() { }
+  // inject service
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   //  (click)="onSelect(hero)"
@@ -23,4 +26,8 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
